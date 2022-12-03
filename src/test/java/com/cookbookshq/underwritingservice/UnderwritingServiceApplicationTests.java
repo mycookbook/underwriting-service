e98f0916b9819a5062031df204372a35843b7900
fokosun <okosunuzflorence@gmail.com>
2022-12-03 13:14:15 -0500

    initial install and commit

---
+++ src/test/java/com/cookbookshq/underwritingservice/UnderwritingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookbookshq.underwritingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderwritingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
